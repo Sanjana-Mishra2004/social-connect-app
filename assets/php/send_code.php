1606691633e9f71b655847bae684ef31c1f4a9dc
Sanjana Mishra <sanjanamishra2004@gmail.com>
2025-04-12 13:32:46 +0530

    Hide credentials and updated .gitignore

--- assets/php/send_code.php
+++ assets/php/send_code.php
@@ -7,25 +7,28 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
+$config = include('mailerconfig.php');
+
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 
 function sendCode($email,$code){
     global $mail;
+    global $config;
 try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'sanjanaproject2024@gmail.com';                     //SMTP username
-    $mail->Password   = 'tkatuhegkucyjvnm';                               //SMTP password
+    $mail->Username   = $config['mail_uname'];                     //SMTP username
+    $mail->Password   = $config['mail_pwd'];                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('sanjanaproject2024@gmail.com', 'MyCollegeConnect');
+    $mail->setFrom($config['mail_uname'], 'MyCollegeConnect');
     $mail->addAddress($email);           
  
     //Content
